6049cedca74db169f3eb6004108826e16c1ef626
renhonglei <renhongleiz@126.com>
2017-12-22 15:05:05 +0800

    update matlab code

--- codes/matlab/match_gene_name.m
+++ codes/matlab/match_gene_name.m
@@ -1,11 +1,12 @@
-function [gene_indexs, gene_names] = match_gene_name()
+function [gene_indexs, gene_names] = match_gene_name(target_gene_idxs,fname)
+global base_path
 gene_idx_filepath = '../../global_files/gene_idx.txt';
 [gidxs, gene_names] = textread(gene_idx_filepath,'%d\t%s');
-target_gene_idxs = load('significant_genes.ind');
+%target_gene_idxs = load('significant_genes.ind');
 target_gene_names = gene_names(target_gene_idxs);
-fid = fopen('target_gene_index_and_names.txt','w');
+fid = fopen(strcat(base_path,fname),'w');
 for i=1:length(target_gene_idxs)
-    fprintf(fid,'%d\t%s\n',target_gene_idxs(i,1),char(target_gene_names{i,:}));
+    fprintf(fid,'%d\t%d\t%s\n',i,target_gene_idxs(i,1),char(target_gene_names{i,:}));
 end
 fclose(fid);
 end
