85662f5bd421815ffd6d5a68d164381b94c35cdd
renhonglei <renhongleiz@126.com>
2018-02-25 20:04:31 +0800

    plot sample_entropy_distribution

--- codes/matlab/plot_methy_sample_entropy_distribution.m
+++ codes/matlab/plot_methy_sample_entropy_distribution.m
@@ -5,31 +5,43 @@ close all;
 
 sample_entropy_dir= '../../data/intermediate_file/methy_sample_entropy/merged_stage/';
 cancer_names = {'BRCA'; 'COAD'; 'KIRC'; 'KIRP'; 'LIHC'; 'LUAD'; 'LUSC'; 'THCA'};
-stage_names = {'n';'i';'ii';'iii';'iv'};
+stage_names = {'normal';'i';'ii';'iii';'iv'};
 ncol = 4;
 nrow = 2;
 figure_dir = '../../figures/sample_entropy/';
-
 if ~exist(figure_dir)
     mkdir(figure_dir);
 end
 
 fig=figure(1);
 clf();
-min_entropy = 1.0;
-max_entropy = 4.0;
+dx=0.04;
+min_entropy = 1.5;
+max_entropy = 3.0;
 for i= 1 : length(cancer_names)
     cancer_name = char(cancer_names(i));
     subplot(nrow, ncol, i);
     hold on;
-    sample_entropy= load(strcat(sample_entropy_dir, cancer_name, '_matlab_sample_entropy.dat'));
-    plot(sample_entropy(:, 1), sample_entropy(:, 2),'b.');
+    for j = 1 : length(stage_names)
+        sample_entropy= load(strcat(sample_entropy_dir, cancer_name, '_', char(stage_names(j)),'_sample_entropy.dat'));
+        [u, x]=hist(sample_entropy(:,2), min_entropy: dx: max_entropy);
+        plot(x, u/(dx*sum(u)));
+    end
+    if i == 2
+       legend({'n','i','ii','iii','iv'},'Box','off');
+    end
     title(cancer_name);
     box on;
-    set(gca, 'XTick', [1 2 3 4 5]);
-    set(gca,'XTicklabel', stage_names);
-    axis([0 5.5 min_entropy max_entropy]);
+    axis([min_entropy max_entropy 0.0 8.0]);
+    if i > 4
+        xlabel('entropy');
+    end
+    if i==1 || i == 5
+        ylabel('cases (%)');
+    end
 end
-fig_save_path = strcat(figure_dir, 'sample_entropy_scatter.eps');
+fig_save_path = strcat(figure_dir, 'sample_entropy_distribution.eps');
 exportfig(fig, fig_save_path , 'FontMode', 'fixed', 'color', 'cmyk','width',8, 'height', 4, 'FontSize', 12,'Resolution',300,'LineWidth',0.5);
 close all;
+   
+
